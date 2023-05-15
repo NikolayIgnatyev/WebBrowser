ab0bc5ba6060bee71cd533c1f9b9e2557c9f8990
Slavan-ST <slavan990900000@gmail.com>
2023-05-15 14:46:43 +0700

    add
    razmetka

---
+++ WebBrowser/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBrowser.ViewModels
+{
+    public class MainWindowViewModel:BaseViewModel
+    {
+        string _proc;
+        string _video;
+        string _volumeRam;
+
+
+        string Proc
+        {
+            get { return _proc; }
+            set 
+            { 
+                _proc = value; 
+                OnPropertyChanged(nameof(Proc));
+            }
+        }
+        string Video
+        {
+            get
+            {
+                return _video;
+            }
+            set
+            {
+                _video = value;
+                OnPropertyChanged(nameof(Video));
+            }
+        }
+        string VolumeRam
+        {
+            get
+            {
+                return _volumeRam;
+            }
+            set
+            {
+                _volumeRam = value;
+                OnPropertyChanged(nameof(VolumeRam));
+            }
+        }
+    }
+}
