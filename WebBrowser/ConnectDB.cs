e19c06b96dcf65e1b147b9388a6506f783e6c6a2
Slavan-ST <slavan990900000@gmail.com>
2023-05-16 14:38:36 +0700

    create connect
    con

---
+++ WebBrowser/ConnectDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace WebBrowser
+{
+    public class ConnectDB
+    {
+        public static string connectionString = "Host = localhost; Port = 5432; Database = postgres; Username = postgres; Password = 123;";
+        static public bool Connect(string connectionString)
+        {
+            try
+            {
+                using(NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                }
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+        
+    }
+}
